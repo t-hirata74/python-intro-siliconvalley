b237a0ab1d209de1a44ab372cdc8950ea45bdf6b
Tomoaki Hirata <t.hirata.0463@gmail.com>
2020-07-21 18:36:59 +0900

    commit

--- lesson.py
+++ lesson.py
@@ -1,1 +1,11 @@
 print('hello world')
+
+def menu(food, *args, **kwargs):
+  """ hogehoge Docstrings """
+  print(food)
+  print(args)
+  print(kwargs)
+
+menu('banana', 'apple', 'orange', entree='beef', drink='coffee')
+
+print(menu.__doc__)
